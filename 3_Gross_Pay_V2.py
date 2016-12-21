69f0bf579545a86518b87a9229f8a88d29e1aa86
sushseqi <sushseqi@gmail.com>
2016-12-22 00:51:13 +0100

    Update 3_Gross_Pay_V2.py

--- 3_Gross_Pay_V2.py
+++ 3_Gross_Pay_V2.py
@@ -1,1 +1,26 @@
 
+# Calculate gross pay as : Pay the hourly rate for the hours up to 35 and 2 times the hourly rate for all hours worked above 35 hours.
+#Program Constants
+baseHours =35.0
+extraPayCoefficient = 2
+
+#function definition
+def computepay(hours,payRate):
+
+    if hours <= baseHours :
+        totalPay = hours*payRate
+    else :
+        totalPay = baseHours*payRate + extraPayCoefficient*(hours - baseHours)*payRate
+
+    return totalPay
+
+#Input data
+try :
+	hours = float(raw_input("Enter worked hours please:"))
+	payRate = float(raw_input("Enter pay rate to use please:"))
+except:
+	print "Please enter valid data"
+	quit()
+
+totalPay = computepay(hours,payRate)
+print totalPay
