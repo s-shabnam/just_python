491d799ec4c90fdbf5def66436bba39d1defbdb9
sushseqi <sushseqi@gmail.com>
2017-01-15 03:18:20 +0100

    Add files via upload

---
+++ 8_Extract_Email_Adress.py
@@ -0,0 +1,23 @@
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "./src/mbox-short.txt"
+try :
+    file = open(fname)
+except :
+    print ('No such file')
+    exit()
+
+emailList = list()
+count = 0
+lineList = list(file)
+for line in lineList:
+    if line.startswith('From'):
+        lineWords = line.split()
+        email = lineWords[1]
+        if email not in emailList:
+            emailList.append(email)
+            print (email)
+            count += 1
+
+emailList.sort()
+# print (emailList)
+print ("There were", count, "lines in the file with From as the first word")
